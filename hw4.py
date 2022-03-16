243809377ca2a72e44147f5525d1ffc66fc7130c
DaniyarNaamatov <100943927+DaniyarNaamatov@users.noreply.github.com>
2022-03-16 16:37:10 +0600

    daniyartest1

--- hw4.py
+++ hw4.py
@@ -3,19 +3,19 @@ screen = turtle.Screen()
 draw = turtle.Turtle()
 screen.bgcolor("black")
 draw.pencolor("red")
-draw.pensize(3)
-draw1 = 0
-draw2 = 0
+draw.pensize(1)
+coor1= 0
+coor2 = 0
 draw.speed(100)
 draw.goto(1, 200)
 draw.pendown()
 
 while True:
 
-    draw.forward(draw1)
-    draw.right(draw2)
-    draw1 += 3
-    draw2 += 1
+    draw.forward(coor1)
+    draw.right(coor2)
+    coor1 += 3
+    coor2 += 1
 
-    if draw2 == 210:
+    if coor2 == 210:
         break
